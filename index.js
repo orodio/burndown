8da86366b7e6d69ff61c00e61c1a2e7c108bc169
James Bjorkman <james@bjorkman.ca>
2014-10-16 10:17:31 +1100

    Init

---
+++ index.js
@@ -0,0 +1,23 @@
+//* @jsx React.DOM
+// jshint node:true, strict:true, eqeqeq:true, freeze:true, newcap:false
+// jshint nonbsp:true, immed:true, indent:2, latedef:false, quotmark:double
+// jshint unused:true, maxdepth:3, maxlen:80, maxcomplexity:5
+
+"use strict";
+
+var app         = require("express")();
+var compression = require("compression");
+var sp          = __dirname + "/static/";
+var port        = Number( 5000 || process.env.PORT );
+
+app.use(compression());
+
+function send(name) {
+  return function(x, res) {
+    res.sendFile(sp + name);
+  };
+}
+
+app.get("/", send("index.html"));
+
+app.listen(port, console.log.bind(null, "Port: " + port));
